d0dc60416e2b0a6fd2ac6e3e244ece58ef5568dd
Kai Marshall <k.marshall@setonhill.edu>
2020-02-14 17:54:03 -0500

    This is my assignment i don't know what is wrong with it it keeps giving me an error and i tried multiple times to do it the right way i will talk with you more about it on monday

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var blank: UITextField!
+    @IBOutlet weak var blank2: UITextField!
+    @IBOutlet weak var blank3: UITextField!
+    @IBOutlet weak var blank4: UITextField!
+    @IBOutlet weak var blank5: UITextField!
+    @IBOutlet weak var blank6: UITextField!
+    @IBOutlet weak var blank7: UITextField!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    
+    @IBAction func Press(_ sender: Any) {
+        Label.text = "First you take your" + blank.text!; + "then add a layer of" + blank2.text!; +" before you pour on a hearty dose of"; +blank3.text! + "next press some" ;+blank4.text! + "down into the"; + blank5.text! + "covering with a sprinkle of"; + blank6.text! + "Thats how i make a"; + blank7.text! + ".";
+    
+        
+    }
+    
+    
+ override func viewDidLoad() {
+       
+    super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
+
+
 
